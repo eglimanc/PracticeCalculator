5a939501c0e93f2fc19c39d97277c40f0d6147fa
eglimanc <egle.netiksyte@gmail.com>
2020-01-14 21:43:23 +0200

    add two numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int num1, num2, sum;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter first number:");
+        num1 = sc.nextInt();
+
+        System.out.println("Please enter second number:");
+        num2 = sc.nextInt();
+
+        sum = num1 + num2;
+        System.out.println(sum);
+        
+        sc.close();
+
     }
 }
