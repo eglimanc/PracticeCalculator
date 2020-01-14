d78b7ea88a29600181e558441cf7d6ef22ef2797
eglimanc <egle.netiksyte@gmail.com>
2020-01-14 22:12:17 +0200

    addition and subtraction methods

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Calculator {
+
+    // constructor for Calculator
+    public Calculator(){}
+
+    //Method for adding two numbers is created below:
+    public int add_numbers (int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public int subtract_numbers (int num1, int num2) {
+       int sub = num1 - num2;
+       return sub;
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int num1, num2, sum;
+        Calculator kalkuliatorius = new Calculator();
+
+        int num1;
+        int num2;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter first number:");
@@ -15,9 +18,11 @@ public class Main {
         System.out.println("Please enter second number:");
         num2 = sc.nextInt();
 
-        sum = num1 + num2;
-        System.out.println(sum);
-        
+        int addition_result = kalkuliatorius.add_numbers(num1, num2);
+        int subtraction_result = kalkuliatorius.subtract_numbers(num1, num2);
+
+        System.out.println("Sum: " + addition_result + " Sub: " + subtraction_result);
+
         sc.close();
 
     }
