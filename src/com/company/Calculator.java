ff62ab498e38c07c88c9fd4ca666d9028d119075
eglimanc <egle.netiksyte@gmail.com>
2020-01-15 21:53:51 +0200

    multiplication and division added

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -3,17 +3,34 @@ package com.company;
 public class Calculator {
 
     // constructor for Calculator
-    public Calculator(){}
+    public Calculator() {
+    }
 
     //Method for adding two numbers is created below:
-    public int add_numbers (int num1, int num2) {
+    public int add_numbers(int num1, int num2) {
         int sum = num1 + num2;
         return sum;
     }
 
-    public int subtract_numbers (int num1, int num2) {
-       int sub = num1 - num2;
-       return sub;
+    // Method for subtraction is created below:
+    public int subtract_numbers(int num1, int num2) {
+        int sub = num1 - num2;
+        return sub;
+    }
+
+    public int multiply_numbers(int num1, int num2) {
+        int multi = num1 * num2;
+        return multi;
+    }
+
+    public int divide_numbers(int num1, int num2) {
+        if (num2 == 0) {
+            return 0;
+        }
+        int div = num1 / num2;
+        return div;
+
 
     }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,22 +6,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Calculator kalkuliatorius = new Calculator();
+        Calculator skaiciuotuvas = new Calculator();
 
-        int num1;
-        int num2;
+        int num1 = 0;
+        int num2 = 0;
+        char operator;
+
+        int result = 0;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter first number:");
-        num1 = sc.nextInt();
+        try {
+            num1 = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("Ivestas ne skaicius");
+        }
 
         System.out.println("Please enter second number:");
         num2 = sc.nextInt();
 
-        int addition_result = kalkuliatorius.add_numbers(num1, num2);
-        int subtraction_result = kalkuliatorius.subtract_numbers(num1, num2);
+        System.out.println("Please enter an operation you would like to perform:");
+        operator = sc.next().charAt(0);
 
-        System.out.println("Sum: " + addition_result + " Sub: " + subtraction_result);
+        switch (operator) {
+            case '+':
+                result = skaiciuotuvas.add_numbers(num1, num2);
+                break;
+            case '-':
+                result = skaiciuotuvas.subtract_numbers(num1, num2);
+                break;
+            case '*':
+                result = skaiciuotuvas.multiply_numbers(num1, num2);
+                break;
+            case '/':
+                result = skaiciuotuvas.divide_numbers(num1, num2);
+                break;
+        }
+
+//        int addition_result = skaiciuotuvas.add_numbers(num1, num2);
+//        int subtraction_result = skaiciuotuvas.subtract_numbers(num1, num2);
+//        int multiplication_result = skaiciuotuvas.multiply_numbers(num1, num2);
+//        int division_result = skaiciuotuvas.divide_numbers(num1, num2);
+
+        System.out.println("Operation result: " + result);
 
         sc.close();
 
